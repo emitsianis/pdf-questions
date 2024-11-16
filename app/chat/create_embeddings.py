db4144b9f38dff05f35042a4b0cf25136a351f18
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-11-16 11:59:00 +0200

    Add: Pinecone, worker(celery), redis

--- app/chat/create_embeddings.py
+++ app/chat/create_embeddings.py
@@ -1,6 +1,8 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 
+from app.chat.vector_stores.pinecone import vector_store
+
 
 def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     text_splitter = RecursiveCharacterTextSplitter(
@@ -10,3 +12,12 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
 
     loader = PyPDFLoader(pdf_path)
     docs = loader.load_and_split(text_splitter)
+
+    for doc in docs:
+        doc.metadata = {
+            "page": doc.metadata["page"],
+            "text": doc.page_content,
+            "pdf_id": pdf_id,
+        }
+
+    vector_store.add_documents(docs)
