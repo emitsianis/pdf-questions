59e2f475b7d33d71da9cc4e6fcec405f4524ffee
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-11-26 18:24:11 +0200

    Add: Weighted random seleciton of components

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -6,6 +6,7 @@ from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 from app.chat.llms import llm_map
 from app.chat.memories import memory_map
 from app.chat.models import ChatArgs
+from app.chat.score import random_component_by_score
 from app.chat.vector_stores import retriever_map
 from app.web.api import get_conversation_components, set_conversation_components
 
@@ -20,7 +21,7 @@ def select_component(
         builder = component_map[previous_component]
         return previous_component, builder(chat_args)
     else:
-        random_name = random.choice(list(component_map.keys()))
+        random_name = random_component_by_score(component_type, component_map)
         builder = component_map[random_name]
         return random_name, builder(chat_args)
 
